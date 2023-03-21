0f2b5ff5d7114a14092f8b6a4e2fcc17d8b5e924
OH_WOW <s.a_ln@mail.ru>
2023-03-21 13:28:52 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "Rus",
+"-2", "computer science", "Russia", "Denmark", "Kazan", "GB", "GBU" };
+
+int count = ArrayCheck(array);
+string[] resultArray = FillResultArray(array, count);
+int ArrayCheck(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+string[] FillResultArray(string[] array, int count)
+{
+    string[] result = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[result.Length - count] = array[i];
+            count--;
+        }
+    }
+    return result;
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}]");
+}
+PrintArray(resultArray);
